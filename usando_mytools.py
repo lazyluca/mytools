37a2e45f843e8fa2580f666207b23536ab378969
lazyluca <al.luca.fujii@impatech.edu.br>
2025-08-11 14:44:29 -0300

    MYTOOLS

---
+++ usando_mytools.py
@@ -0,0 +1,6 @@
+import MYTOOLS
+
+n = int(input("Insira um número de casas decimais: "))
+
+print("Pi é " + MYTOOLS.pi_real(n) + f"{n} casas decimais")
+print("E é " + MYTOOLS.e_real(n) + f"{n} casas decimais")
